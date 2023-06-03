b92160fea62c0d307e4ca8a5203848736d49e7b3
AkshayShineKrishna <ask007kichu@gmail.com>
2023-06-03 09:07:49 +0530

    changed endpoints

--- main.py
+++ main.py
@@ -4,76 +4,87 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-
 @app.route('/get_upcoming', methods=['GET'])
-def get_filepaths():
-    # Retrieve the API key from the request parameters
+def get_upcoming():
+    # Retrieve the 'api_key' query parameter from the request
     api_key = request.args.get('api_key')
 
-    # Function to fetch the data from a given URL
+    # Get the current date and time
+    current_date = datetime.now()
+
     def fetch_data(url):
+        # Function to make a GET request to the provided URL and return the JSON response
         response = requests.get(url)
         if response.status_code == 200:
             return response.json()
         else:
             return None
 
-    # First API call to retrieve upcoming movies
-    upcoming_url = f'https://api.themoviedb.org/3/movie/upcoming?api_key={api_key}'
-    upcoming_data = fetch_data(upcoming_url)
-    print(upcoming_data)
+    # Construct the base URL for the movie database API with the provided 'api_key'
+    base_url = f'https://api.themoviedb.org/3/discover/movie?api_key={api_key}'
 
-    if upcoming_data is not None:
-        # Extracting the movie IDs from the first JSON response
+    # Format the current date components to two digits
+    current_day = str(current_date.day).zfill(2)
+    current_month = str(current_date.month).zfill(2)
+    current_year = current_date.year
+
+    # Calculate the next month and year, considering the special case for December
+    next_month = int(current_month) + 2 if current_month != '12' else '01'
+    next_month = str(next_month).zfill(2)
+    next_year = current_year if current_month != '12' else str(current_date.year + 1)
+
+    # Construct the conditions strings for the API requests
+    conditions = f"&primary_release_date.gte={current_year}-{current_month}-{current_day}" \
+                   f"&primary_release_date.lte={next_year}-{next_month}-31"
+
+    # Fetch data using the first conditions
+    url = base_url + conditions
+    upcoming_data = fetch_data(url)
+
+
+    if upcoming_data is not None :
         movie_ids = [movie['id'] for movie in upcoming_data['results']]
         results = []
 
         # Second API call to retrieve images for each movie
         images_base_url = f'https://api.themoviedb.org/3/movie/{{}}/images?api_key={api_key}'
 
-        current_date = datetime.now()
-
         for movie_id in movie_ids:
             images_url = images_base_url.format(movie_id)
             images_data = fetch_data(images_url)
-            print(images_data)
             if images_data is not None and 'logos' in images_data:
                 # Finding the first filepath that ends with '.png' in the 'logos' field
                 filepath = next(
                     (logo['file_path'] for logo in images_data['logos'] if logo['file_path'].endswith('.png')), None)
             else:
                 filepath = None
-
             # Extracting the month and day from the 'release_date'
             release_date = upcoming_data['results'][movie_ids.index(movie_id)]['release_date']
             release_date_obj = datetime.strptime(release_date, '%Y-%m-%d')
+            month = release_date_obj.strftime('%b').upper()
+            day = release_date_obj.strftime('%d')
 
-            # Add the movie to the list only if the release date is greater than the current date
-            if release_date_obj > current_date or release_date_obj == current_date:
-                month = release_date_obj.strftime('%b').upper()
-                day = release_date_obj.strftime('%d')
-                if len(day) == 1:
-                    day = '0' + day
+            # Third API call to retrieve site and key
+            videos_url = f"https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key={api_key}"
+            videos_data = fetch_data(videos_url)
 
-                # Third API call to retrieve site and key
-                videos_url = f"https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key={api_key}"
-                videos_data = fetch_data(videos_url)
+            if videos_data is not None and 'results' in videos_data:
+                trailer_videos = [video for video in videos_data['results'] if
+                                  video['type'] in ['Trailer', 'Teaser'] and video['site'].lower() == 'youtube']
 
-                if videos_data is not None and 'results' in videos_data:
-                    trailer_videos = [video for video in videos_data['results'] if
-                                      video['type'] in ['Trailer', 'Teaser'] and video['site'].lower() == 'youtube']
-
-                    if trailer_videos:
-                        # Append 'https://youtu.be/' to the key
-                        key = 'https://youtu.be/' + trailer_videos[0]['key']
-                        site = 'YouTube'
-                    else:
-                        key = None
-                        site = None
+                if trailer_videos:
+                    # Append 'https://youtu.be/' to the key
+                    key = 'https://youtu.be/' + trailer_videos[0]['key']
+                    site = 'YouTube'
                 else:
                     key = None
                     site = None
+            else:
+                key = None
+                site = None
 
+            if filepath is not None and upcoming_data['results'][movie_ids.index(movie_id)][
+                'backdrop_path'] is not None:
                 # Prepare movie data with file paths and separate keys for month and day
                 movie_data = {
                     'id': movie_id,
@@ -89,7 +100,6 @@ def get_filepaths():
                     'key': key
                 }
                 results.append(movie_data)
-
         response_data = {'results': results}
     else:
         response_data = {'results': []}
